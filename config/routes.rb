7b6858324fd5dfddeaa819744bb3024ded41b4ad
Florian Cousin <floriancousin@hotmail.com>
2016-08-11 13:12:37 +0200

    added controller et view for cocktail and dose

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:new, :index, :create, :show] do
+    resources :doses, only: [:new, :create, :show, :destroy]
+  end
+    root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
